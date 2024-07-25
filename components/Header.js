5b0de9b6f44e8e8b30be3d58ad3ac80153e524f1
RomanOstrous <ostrous.roman@gmail.com>
2024-07-26 00:56:36 +0300

    add standart folders

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      Heder
+    </div>
+  )
+}
+
+export default Header
---
+++ pages/gallery-photos/photos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Photos = () => {
+  return (
+    <div>
+      Сторінка фоток
+    </div>
+  )
+}
+
+export default Photos
